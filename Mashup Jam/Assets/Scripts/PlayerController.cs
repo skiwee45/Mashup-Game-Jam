f949edd65639b5fa39461213e85e5413cbfcc79d
Blue Putty <65861156+blue-putty@users.noreply.github.com>
2021-06-18 20:23:31 -0400

    added player controls

--- Mashup Jam/Assets/Scripts/PlayerController.cs
+++ Mashup Jam/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 100;
+    public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 transformVector = new Vector3(10, 0, 0);
+        transformVector = transformVector.normalized * speed;
+        rb.MovePosition(rb.transform.position + transformVector);
     }
 }
