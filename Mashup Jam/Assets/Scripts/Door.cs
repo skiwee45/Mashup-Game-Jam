d2148dcbdcb6a38e08bc5c3488e2172c2d638e8f
Raymond Lin <raymond.lin.05@gmail.com>
2021-06-19 18:55:48 -0400

    Created door prefab and door collision works

--- Mashup Jam/Assets/Scripts/Door.cs
+++ Mashup Jam/Assets/Scripts/Door.cs
@@ -1,18 +1,12 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Sent when an incoming collider makes contact with this object's collider (2D physics only).
+	protected void OnCollisionEnter2D(Collision2D collisionInfo)
+	{
+		Debug.Log("Collider Works");
+	}
 }
