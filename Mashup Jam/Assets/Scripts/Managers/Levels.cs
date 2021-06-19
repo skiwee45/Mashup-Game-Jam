a1b4ef20508975ce49cb8e00bf112d3bdbb829d7
Blue Putty <65861156+blue-putty@users.noreply.github.com>
2021-06-19 15:31:40 -0400

    commented level

--- Mashup Jam/Assets/Scripts/Managers/Levels.cs
+++ Mashup Jam/Assets/Scripts/Managers/Levels.cs
@@ -8,7 +8,7 @@ public class Levels : Singleton<Levels>
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadScene("Level1");
+        // SceneManager.LoadScene("Level1");
         // DontDestroyOnLoad();
     }
 
